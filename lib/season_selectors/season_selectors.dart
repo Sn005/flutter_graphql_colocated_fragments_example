9d6a2c497d99de6ea5e62f508b6ae087d7978eb9
Sn005 <living-inthe-junktown@hotmail.co.jp>
2022-07-04 23:42:18 +0900

    refactor: widgetをセレクター毎に分割

--- lib/season_selectors/season_selectors.dart
+++ lib/season_selectors/season_selectors.dart
@@ -18,39 +18,65 @@ class SeasonSelectors extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: [
-      DropdownButton<int>(
-        value: variables.seasonYear,
-        items: yearRange.map<DropdownMenuItem<int>>((value) {
-          return DropdownMenuItem<int>(
-            value: value,
-            child: Text(value.toString()),
-          );
-        }).toList(),
-        onChanged: (int? value) {
-          if (value != null) {
-            print(value);
-          }
-        },
-      ),
-      DropdownButton<Enum$MediaSeason>(
-        value: variables.season,
-        items: <Enum$MediaSeason>[
-          Enum$MediaSeason.WINTER,
-          Enum$MediaSeason.SPRING,
-          Enum$MediaSeason.SUMMER,
-          Enum$MediaSeason.FALL
-        ].map<DropdownMenuItem<Enum$MediaSeason>>((Enum$MediaSeason value) {
-          return DropdownMenuItem<Enum$MediaSeason>(
-            value: value,
-            child: Text(value.toString().split('.').last),
-          );
-        }).toList(),
-        onChanged: (Enum$MediaSeason? value) {
-          if (value != null) {
-            print(value);
-          }
-        },
-      ),
+      SeasonYearSelector(seasonYear: variables.seasonYear),
+      SeasonSelector(season: variables.season),
     ]);
+  }
+}
+
+class SeasonYearSelector extends StatelessWidget {
+  final int? seasonYear;
+  const SeasonYearSelector({
+    Key? key,
+    this.seasonYear,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<int>(
+      value: seasonYear,
+      items: yearRange.map<DropdownMenuItem<int>>((value) {
+        return DropdownMenuItem<int>(
+          value: value,
+          child: Text(value.toString()),
+        );
+      }).toList(),
+      onChanged: (int? value) {
+        if (value != null) {
+          print(value);
+        }
+      },
+    );
+  }
+}
+
+class SeasonSelector extends StatelessWidget {
+  final Enum$MediaSeason? season;
+  const SeasonSelector({
+    Key? key,
+    this.season,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<Enum$MediaSeason>(
+      value: season,
+      items: <Enum$MediaSeason>[
+        Enum$MediaSeason.WINTER,
+        Enum$MediaSeason.SPRING,
+        Enum$MediaSeason.SUMMER,
+        Enum$MediaSeason.FALL
+      ].map<DropdownMenuItem<Enum$MediaSeason>>((Enum$MediaSeason value) {
+        return DropdownMenuItem<Enum$MediaSeason>(
+          value: value,
+          child: Text(value.toString().split('.').last),
+        );
+      }).toList(),
+      onChanged: (Enum$MediaSeason? value) {
+        if (value != null) {
+          print(value);
+        }
+      },
+    );
   }
 }
