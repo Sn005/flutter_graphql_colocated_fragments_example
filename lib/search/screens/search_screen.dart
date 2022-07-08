1795a263e036769a5d19145f57cb9314d1d06167
Sn005 <living-inthe-junktown@hotmail.co.jp>
2022-07-08 16:59:27 +0900

    fix: MediaListをスクロール対応

--- lib/search/screens/search_screen.dart
+++ lib/search/screens/search_screen.dart
@@ -5,10 +5,34 @@ import '../widgets/media_list/index.dart';
 import '../widgets/search_screen_query_variables_selectors/index.dart';
 import 'use_search_screen.dart';
 
-class SearchScreen extends HookConsumerWidget {
+class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("title"),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              SearchScreenQueryVariablesSelectors(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: SizedBox(
+                    child: SearchScreenMediaList(),
+                  ),
+                ),
+              ),
+            ]));
+  }
+}
+
+class SearchScreenMediaList extends HookConsumerWidget {
+  const SearchScreenMediaList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -18,24 +42,12 @@ class SearchScreen extends HookConsumerWidget {
     }
 
     if (result.isLoading) {
-      return const Text('Loading');
+      return const Text('Loading...');
     }
     final data = result.data;
     final page = data?.Page;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("title"),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SearchScreenQueryVariablesSelectors(),
-          MediaList(
-              data: Fragment$MediaList(
-                  media: page?.media, $__typename: page!.$__typename)),
-        ],
-      )),
-    );
+    return MediaList(
+        data: Fragment$MediaList(
+            media: page?.media, $__typename: page!.$__typename));
   }
 }
